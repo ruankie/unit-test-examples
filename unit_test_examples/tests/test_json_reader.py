fe05fdda7d80ed5f7e57be383532845e53a328a1
Ruan Pretorius <ruan@Ruans-MacBook-Pro.local>
2022-07-05 21:11:24 +0200

    added mock tests for reading json files

--- unit_test_examples/tests/test_json_reader.py
+++ unit_test_examples/tests/test_json_reader.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from unittest import mock
 import unit_test_examples.tools.json_reader as jr
 
 # set working dir to the location of this script
@@ -9,13 +10,42 @@ class TestJsonReader(unittest.TestCase):
     """Tests various functions in the json_reader module.
     """
 
-    def test_reading_json_file(self):
-        json_data = jr.read_json('../data/data1.json')
-        self.assertEqual(json_data['fruit'], 'Apple')
+    def setUp(self):
+        self.LARGE_JSON_CONTENTS = {
+            "quiz": {
+                "sport": {
+                    "q1": {
+                        "question": "Which one is correct team name in NBA?",
+                        "options": [
+                            "New York Bulls",
+                            "Los Angeles Kings",
+                            "Golden State Warriros",
+                            "Huston Rocket"
+                        ],
+                        "answer": "Huston Rocket"
+                    }
+                }
+            }
+        }
 
-        json_data = jr.read_json('../data/data2.json')
-        self.assertEqual(json_data['quiz']['maths']['q2']['answer'], '4')
-        
+    def tearDown(self):
+        pass
+
+
+    @mock.patch('json.load', return_value={
+        "fruit": "Orange",
+        "size": "Medium",
+        "color": "Orange"
+    })
+    def test_reading_simple_json_file(self, mock_json):
+        json_data = jr.read_json('../data/data1.json')
+        self.assertEqual(json_data['fruit'], 'Orange')
+        self.assertEqual(json_data['size'], 'Medium')
+
+    def test_reading_larger_json_file(self):
+        with mock.patch('json.load', return_value=self.LARGE_JSON_CONTENTS):
+            json_data = jr.read_json('../data/data2.json')
+            self.assertEqual(json_data['quiz']['sport']['q1']['answer'], 'Huston Rocket')
 
 
 if __name__ == '__main__':
