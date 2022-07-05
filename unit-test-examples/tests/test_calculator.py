e95f015b4ac4b6dee35b27349c6d7f37a7736127
Ruan Pretorius <ruan@Ruans-MacBook-Pro.local>
2022-07-05 18:43:47 +0200

    added empty tests

--- unit-test-examples/tests/test_calculator.py
+++ unit-test-examples/tests/test_calculator.py
@@ -1,2 +1,17 @@
 import unittest
-from unittest import TestCase
+
+class TestCalculator(unittest.TestCase):
+    """For testing various methods in the Calculator class.
+    """
+
+    def test_addition(self):
+        pass
+
+    def test_subtraction(self):
+        pass
+
+    def test_multiplication(self):
+        pass
+
+    def test_division(self):
+        pass
